80c80a9051796218f653296e17f8fd2bca740f6f
Yuriy Chaban <Yuriy Chaban>
2016-11-15 17:00:35 +0200

    add modal block to all page

--- js/main.js
+++ js/main.js
@@ -100,4 +100,18 @@ $('.center').slick({
 nextArrow: '<i class="fa fa-long-arrow-right"></i>',
 prevArrow: '<i class="fa fa-long-arrow-left"></i>',
 });
-
+// for annation scroll
+$(document).ready(function() {
+   $('a[href="#"]').bind('click', function(e) {
+  e.preventDefault(); //prevent the "normal" behaviour which would be a "hard" jump
+       
+  var target = $(this).attr("href"); //Get the target
+      
+  // perform animated scrolling by getting top-position of target-element and set it as scroll target
+  $('html, body').stop().animate({ scrollTop: $(target).offset().top }, 1000, function() {
+       location.hash = target;  //attach the hash (#jumptarget) to the pageurl
+  });
+      
+  return false;
+   });
+});
